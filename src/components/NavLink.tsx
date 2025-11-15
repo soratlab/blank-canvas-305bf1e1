37684b23c3e96505b0cd8e1d6e2f71176dc9cab8
soratlab <sacmeuarmarium@gmail.com>
2025-11-15 15:42:06 -0300

    Update NavLink.tsx

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,28 +1,16 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
-import { forwardRef } from "react";
-import { cn } from "@/lib/utils";
+import React from "react";
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
-  className?: string;
-  activeClassName?: string;
-  pendingClassName?: string;
+export default function NavLink({ href, children, onClick }: any) {
+  return (
+    <a
+      href={href}
+      onClick={onClick}
+      className="
+        font-sora text-[15px] text-black hover:text-brand-yellow
+        transition cursor-pointer
+      "
+    >
+      {children}
+    </a>
+  );
 }
-
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
-    return (
-      <RouterNavLink
-        ref={ref}
-        to={to}
-        className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
-        }
-        {...props}
-      />
-    );
-  },
-);
-
-NavLink.displayName = "NavLink";
-
-export { NavLink };
