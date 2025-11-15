bcdeb7bf5a51d1593d8ee91c8c55a1e3535b4374
soratlab <sacmeuarmarium@gmail.com>
2025-11-15 13:11:35 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import RetentionSection from "@/components/RetentionSection";
 import ReviewsSection from "@/components/ReviewsSection";
+import VideoSection from "@/components/VideoSection";
 
 export default function Index() {
   return (
@@ -8,6 +9,7 @@ export default function Index() {
       <Hero />
       <RetentionSection />
       <ReviewsSection />
+      <VideoSection />
     </main>
   );
 }
